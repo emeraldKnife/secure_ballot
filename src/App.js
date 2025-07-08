3ce5ef6e9c6a6ae22b0c4218964d05ae8392a7c3
MOHIT PATIL <mohitypatil2006@gmail.com>
2025-07-08 12:07:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';           // Import Home page
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />         {/* Home route */}
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
